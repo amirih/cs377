c8023ca863a270d5c3c10aaaa79f4960fd6dab4c
Hossein Amiri <hossein.amiri.enterprise@gmail.com>
2023-09-19 14:07:19 -0400

    check vercel

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,6 @@ export default function Home() {
       setError(data.error);
       setData([]);
       setColumns([]);
-      return;
     }
     const tables = [];
     for (const key in data.tables) {
@@ -82,7 +81,7 @@ export default function Home() {
           <div className="surface-section px-4 py-5 md:px-6 lg:px-8">
             <div className="flex lg:align-items-center flex-column lg:flex-row">
               <div className="text-3xl font-medium text-900 mr-0 lg:mr-4 mb-4 lg:mb-0">
-                CS 377 Emory
+                CS 377 Emory Un
               </div>
               <TabMenu
                 model={items}
