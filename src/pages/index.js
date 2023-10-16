103aed85fd1a4875e362202bf602198a478b6b20
Hossein Amiri <hossein.amiri.enterprise@gmail.com>
2023-10-16 15:45:35 -0400

    down for midterm

--- src/pages/index.js
+++ src/pages/index.js
@@ -204,11 +204,31 @@
 //   );
 // }
 
-// a simple hello world page
-export default function Home() {
+
+// pages/markdownPage.js
+import fs from "fs";
+import path from "path";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import "github-markdown-css/github-markdown.css";
+
+const MarkdownPage = ({ content }) => {
   return (
-    <div>
-      <h1>Hello Midterm</h1>
+    <div className="markdown-body">
+      <ReactMarkdown>{content}</ReactMarkdown>
     </div>
   );
+};
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "solutions/a0.md");
+  const content = fs.readFileSync(filePath, "utf8");
+
+  return {
+    props: {
+      content,
+    },
+  };
 }
+
+export default MarkdownPage;
